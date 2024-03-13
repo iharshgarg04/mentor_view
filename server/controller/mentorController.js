6669de5b980e9cc96c40c62df11b060ce29c94c0
iharshgarg04 <iharshgarg04@gmail.com>
2024-03-14 01:56:33 +0530

    remove Student api

--- server/controller/mentorController.js
+++ server/controller/mentorController.js
@@ -1,4 +1,6 @@
+const Marks = require('../models/marks');
 const Mentor = require('../models/mentor');
+const Student = require('../models/student');
 
 exports.addStudents = async(req,res)=>{
     try{
@@ -28,6 +30,8 @@ exports.addStudents = async(req,res)=>{
             })
         }
 
+        await Student.updateMany({_id:{$in:students}},{assigned:true});
+
         mentor.student.push(...students);
         await mentor.save();
         return res.status(200).json({
@@ -53,9 +57,10 @@ exports.removeStudent = async(req,res)=>{
             })
         }
         
-        //student data to be deleted
 
+        await Student.findByIdAndUpdate(studentId,{assigned:false},{new:true});
         const response = await Mentor.findByIdAndUpdate(mentorId,{$pull:{student:studentId}},{new:true}).populate("student")
+        await Marks.deleteMany({ student: studentId });
          
         return res.status(200).json({
             success:true,
