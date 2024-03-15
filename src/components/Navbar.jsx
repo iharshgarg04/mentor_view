51ffaf95ebe60131b7af986c2abcb53e99a3c6e0
iharshgarg04 <iharshgarg04@gmail.com>
2024-03-15 19:53:41 +0530

    added logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import '../App.css'
+import { Button } from '@mui/material'
+import { useNavigate } from 'react-router'
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const mentorData=localStorage.getItem("mentorData");
   return (
     <div className='Nav-header'>
         {/* <div className='nav-logo'>
@@ -10,6 +14,16 @@ const Navbar = () => {
         <div className='nav-mentor'>
             MentorView
         </div>
+        {mentorData &&
+          <Button sx={{color:"white",backgroundColor:"grey" , '&:hover':{
+            backgroundColor:"black"
+          }}} variant='contained' onClick={()=>{
+            navigate("/");
+            localStorage.removeItem("mentorData");
+          }}>
+            LogOut
+          </Button>
+        }
     </div>
   )
 }
